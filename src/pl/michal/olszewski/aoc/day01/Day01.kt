16b9704789bf64f99717dead68f2877edd3fc925
Michal Olszewski <michal.a.olszewski@allegro.pl>
2021-12-01 07:02:46 +0100

    First day implementation - refactor

--- src/pl/michal/olszewski/aoc/day01/Day01.kt
+++ src/pl/michal/olszewski/aoc/day01/Day01.kt
@@ -4,29 +4,23 @@ import readInput
 
 fun main() {
     fun part1(input: List<Int>): Int {
-        var previousValue = input.first()
-        var result = 0;
-        for (value in input.subList(1, input.size)) {
-            if (value > previousValue) {
-                result++
+        return input.asSequence()
+            .windowed(1)
+            .map { it.sum() }
+            .zipWithNext()
+            .count {
+                it.second > it.first
             }
-            previousValue = value;
-        }
-        return result;
     }
 
     fun part2(input: List<Int>): Int {
-        var result = 0;
-        var firstSum = input.subList(0, 3).sum()
-        val lastIndex = input.size - 3
-        for (i in 1..lastIndex) {
-            val secondSum = input.subList(i, i + 3).sum()
-            if (secondSum > firstSum) {
-                result++
+        return input.asSequence()
+            .windowed(3)
+            .map { it.sum() }
+            .zipWithNext()
+            .count {
+                it.second > it.first
             }
-            firstSum = secondSum
-        }
-        return result;
     }
 
     // test if implementation meets criteria from the description, like:
