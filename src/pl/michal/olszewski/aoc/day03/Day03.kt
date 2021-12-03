c8f753057e071c9254fcb2c834464916fa2bbc2c
Michal Olszewski <michal.a.olszewski@allegro.pl>
2021-12-03 16:46:59 +0100

    3 day implementation - small refactor

--- src/pl/michal/olszewski/aoc/day03/Day03.kt
+++ src/pl/michal/olszewski/aoc/day03/Day03.kt
@@ -2,66 +2,65 @@ package pl.michal.olszewski.aoc.day03
 
 import readInput
 
-fun main() {
-    fun part1(input: List<String>): Int {
-        val length = input.first().length
-        var gammaRateAsString = ""
-        var epsilonRateAsString = ""
-        for (i in 0 until length) {
-            val zeros = input.count { it[i] == '0' }
-            val ones = input.count { it[i] == '1' }
-            if (zeros > ones) {
-                gammaRateAsString += '0'
-                epsilonRateAsString += '1'
-            } else {
-                gammaRateAsString += '1'
-                epsilonRateAsString += '0'
+fun isMoreZeroThanOnes(position: List<Char>) = position.count { it == '0' } > position.count { it == '1' }
+
+fun isLessZerosThanOnes(position: List<Char>) = position.count { it == '0' } <= position.count { it == '1' }
+
+private fun calculateOxygenRating(input: List<String>): String {
+    val length = input.first().length
+    var oxygenInput = input
+    for (i in 0 until length) {
+        oxygenInput = when {
+            isLessZerosThanOnes(oxygenInput.map { it[i] }) -> {
+                oxygenInput.filter { it[i] == '1' }
+            }
+            else -> {
+                oxygenInput.filter { it[i] == '0' }
             }
         }
-        return gammaRateAsString.toInt(2) * epsilonRateAsString.toInt(2)
+
+        if (oxygenInput.size == 1) {
+            return oxygenInput.first()
+        }
+    }
+    throw IllegalStateException()
+}
+
+private fun calculateScrubberRating(input: List<String>): String {
+    val length = input.first().length
+    var scrubberRatingInput = input
+    for (i in 0 until length) {
+        scrubberRatingInput = when {
+            isMoreZeroThanOnes(scrubberRatingInput.map { it[i] }) -> {
+                scrubberRatingInput.filter { it[i] == '1' }
+            }
+            else -> {
+                scrubberRatingInput.filter { it[i] == '0' }
+            }
+        }
+
+        if (scrubberRatingInput.size == 1) {
+            return scrubberRatingInput.first()
+        }
+    }
+    throw IllegalStateException()
+}
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val correspondingPositions = (0 until input.first().length)
+            .map { i -> input.map { it[i] } }
+        val gammaRate = correspondingPositions.map { position -> if (isMoreZeroThanOnes(position)) '0' else '1' }.joinToString(separator = "").toInt(2)
+        val epsilonRate = correspondingPositions.map { position -> if (isMoreZeroThanOnes(position)) '1' else '0' }.joinToString(separator = "").toInt(2)
+
+        return gammaRate * epsilonRate
     }
 
     fun part2(input: List<String>): Int {
-        var oxygenInput: List<String> = input
-        var scrubberRatingInput: List<String> = input
-        val length = input.first().length
-        var oxygenGeneratorRatingAsString = ""
-        var srubberRatingAsString = ""
-        for (i in 0 until length) {
-            val zeros = oxygenInput.count { it[i] == '0' }
-            val ones = oxygenInput.count { it[i] == '1' }
-            if (oxygenInput.size == 1) {
-                break
-            }
-            when {
-                zeros == ones || ones > zeros -> {
-                    oxygenInput = oxygenInput.filter { it[i] == '1' }
-                }
-                else -> {
-                    oxygenInput = oxygenInput.filter { it[i] == '0' }
-                }
-            }
-        }
-        oxygenGeneratorRatingAsString = oxygenInput[0]
-
-        for (i in 0 until length) {
-            val zeros = scrubberRatingInput.count { it[i] == '0' }
-            val ones = scrubberRatingInput.count { it[i] == '1' }
-            if (scrubberRatingInput.size == 1) {
-                break
-            }
-            when {
-                zeros <= ones -> {
-                    scrubberRatingInput = scrubberRatingInput.filter { it[i] == '0' }
-                }
-                else -> {
-                    scrubberRatingInput = scrubberRatingInput.filter { it[i] == '1' }
-                }
-            }
-        }
-        srubberRatingAsString = scrubberRatingInput[0]
-
-        return oxygenGeneratorRatingAsString.toInt(2) * srubberRatingAsString.toInt(2)
+        val oxygenGeneratorRatingAsString = calculateOxygenRating(input)
+        val scrubberRatingAsString = calculateScrubberRating(input)
+        return oxygenGeneratorRatingAsString.toInt(2) * scrubberRatingAsString.toInt(2)
     }
 
     // test if implementation meets criteria from the description, like:
