c3005974870d820db4ec2bf5d5ec4ce968c62991
Michal Olszewski <michal.a.olszewski@allegro.pl>
2021-12-04 11:54:14 +0100

    4 day implementation - after refactor

--- src/pl/michal/olszewski/aoc/day04/Day04.kt
+++ src/pl/michal/olszewski/aoc/day04/Day04.kt
@@ -2,9 +2,12 @@ package pl.michal.olszewski.aoc.day04
 
 import readInput
 
+private const val BOARD_SIZE = 5
+
 data class BingoField(val value: Int, val marked: Boolean = false)
 
 data class BingoFields(val fields: List<BingoField>) {
+
     fun markNumber(numberDrawn: Int): BingoFields {
         return this.copy(
             fields = fields.map {
@@ -14,9 +17,14 @@ data class BingoFields(val fields: List<BingoField>) {
             }
         )
     }
+
+    fun allFieldsAreMarked(): Boolean {
+        return fields.all { it.marked }
+    }
 }
 
 data class BingoBoard(var columns: List<BingoFields>, var rows: List<BingoFields>) {
+
     fun markNumber(numberDrawn: Int) {
         columns = columns.map {
             it.markNumber(numberDrawn)
@@ -27,29 +35,34 @@ data class BingoBoard(var columns: List<BingoFields>, var rows: List<BingoFields
     }
 
     fun isWinning(): Boolean {
-        return columns.any { column -> column.fields.all { field -> field.marked } } || rows.any { row -> row.fields.all { field -> field.marked } }
+        return anyColumnHasAllValuesMarked() || anyRowHasAllValuesMarked()
     }
 
+    private fun anyRowHasAllValuesMarked() = rows.any { row -> row.allFieldsAreMarked() }
+
+    private fun anyColumnHasAllValuesMarked() = columns.any { column -> column.allFieldsAreMarked() }
     fun sumOfUnmarkedFields(): Int {
         return rows.sumOf { it.fields.filter { field -> !field.marked }.sumOf { field -> field.value } }
     }
 }
 
+fun readBingoBoards(input: List<String>): List<BingoBoard> = input.drop(1)
+    .filter { it.isNotEmpty() }
+    .chunked(BOARD_SIZE)
+    .map { board ->
+        val rows = board.map { boardLine -> boardLine.split(" ").filter { it.isNotEmpty() }.map { value -> value.toInt() } }
+        val cols = rows.indices.map { column -> rows.map { it[column] } }
+        BingoBoard(
+            columns = cols.map { BingoFields(it.map(::BingoField)) },
+            rows = rows.map { BingoFields(it.map(::BingoField)) }
+        )
+    }
+
 fun main() {
+
     fun part1(input: List<String>): Int {
         val drawnNumbers = input.take(1).flatMap { it.split(",") }.map { it.toInt() }
-
-        val boardsTMP = input.drop(1)
-            .filter { it.isNotEmpty() }
-            .chunked(5) //TODO const board size
-            .map { board ->
-                val rows = board.map { it.split(" ").filter { it.isNotEmpty() }.map { value -> value.toInt() } }
-                val cols = rows.indices.map { column -> rows.map { it[column] } }
-                BingoBoard(
-                    columns = cols.map { BingoFields(it.map { value -> BingoField(value, false) }) },
-                    rows = rows.map { BingoFields(it.map { value -> BingoField(value, false) }) }
-                )
-            }
+        val boardsTMP = readBingoBoards(input)
 
         drawnNumbers.forEach { numberDrawn ->
             boardsTMP.forEach { board ->
@@ -66,18 +79,7 @@ fun main() {
     fun part2(input: List<String>): Int {
         val winningScores = mutableListOf<Int>()
         val drawnNumbers = input.take(1).flatMap { it.split(",") }.map { it.toInt() }
-
-        val boardsTMP = input.drop(1)
-            .filter { it.isNotEmpty() }
-            .chunked(5) //TODO const board size
-            .map { board ->
-                val rows = board.map { it.split(" ").filter { it.isNotEmpty() }.map { value -> value.toInt() } }
-                val cols = rows.indices.map { column -> rows.map { it[column] } }
-                BingoBoard(
-                    columns = cols.map { BingoFields(it.map { value -> BingoField(value, false) }) },
-                    rows = rows.map { BingoFields(it.map { value -> BingoField(value, false) }) }
-                )
-            }
+        val boardsTMP = readBingoBoards(input)
 
         drawnNumbers.forEach { numberDrawn ->
             boardsTMP
